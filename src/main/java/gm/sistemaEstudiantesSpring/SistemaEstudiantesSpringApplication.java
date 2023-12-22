05df6f3509812d93ab2039b20af4b2e967e6fd34
MartinoRizzo <martingabriel.rizzo@gmail.com>
2023-12-21 21:03:10 -0300

    Proyecto con springboot

---
+++ src/main/java/gm/sistemaEstudiantesSpring/SistemaEstudiantesSpringApplication.java
@@ -0,0 +1,13 @@
+package gm.sistemaEstudiantesSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEstudiantesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEstudiantesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gm/sistemaEstudiantesSpring/SistemaEstudiantesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package gm.sistemaEstudiantesSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEstudiantesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
